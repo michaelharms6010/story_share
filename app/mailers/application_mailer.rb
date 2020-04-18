340a444e1cbf422be4b1850dee02e9b99879e955
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-18 15:08:41 -0400

    Adding mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  require 'mail'
+
+  address = Mail::Address.new("noreply@br80.io")
+  address.display_name = "br80"  # Appears as the sendee's name
+  default from: address.format
+
   layout 'mailer'
 end
