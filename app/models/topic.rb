422b1394092a86ef30b2934ef9d3cf274fdd142b
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-19 12:23:05 -0400

    topic views

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,6 +5,10 @@ class Topic < ActiveRecord::Base
   validates :prompt, presence: true, length: { minimum: 10, maximum: 255 },
                     uniqueness: { case_sensitive: false }
 
+  def length_options
+    ["MICRO", "SHORT", "MEDIUM", "LONG"]
+  end
+
   private
 
 end
