ae3329d538f9fc525d20d06e83a11875572b3aac
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-19 13:40:48 -0400

    Add story views

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Story < ActiveRecord::Base
 
   belongs_to :user
+  belongs_to :topic
 
   default_scope -> { order(created_at: :desc) }
   validates :text, presence: true
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,7 @@
 class Topic < ActiveRecord::Base
 
+  has_many   :stories, dependent: :destroy
+
   validates :prompt, presence: true, length: { minimum: 10, maximum: 255 },
                     uniqueness: { case_sensitive: false }
   validates :length, presence: true
