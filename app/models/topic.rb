7bad622ac6e2ed7d8f5000ed9f88122360ddc2e5
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-19 12:29:53 -0400

    Fix form

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,11 +1,10 @@
 class Topic < ActiveRecord::Base
 
-  default_scope -> { order(published_at: :desc) }
   validates :text, presence: true
   validates :prompt, presence: true, length: { minimum: 10, maximum: 255 },
                     uniqueness: { case_sensitive: false }
 
-  def length_options
+  def self.length_options
     ["MICRO", "SHORT", "MEDIUM", "LONG"]
   end
 
