c9e0c00b5c83c69b6b9fe2248d55278859447e8e
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-19 15:06:01 -0400

    Friendship model

---
+++ app/models/friendship.rb
@@ -0,0 +1,45 @@
+class Friendship < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
+
+  validates_uniqueness_of :user_id, :scope => :friend_id
+  validate :not_friends_with_self
+
+  before_save :set_confirmation
+
+  def self.create_bidirectional_friendship(user, friend)
+    return false if !user.present? || !friend.present?
+    profile_id = user.block_game_profile.id
+    friend_profile_id = friend.block_game_profile.id
+    if Friendship.create(user_id: user.id, friend_id: friend.id, block_game_profile_id: profile_id, friend_block_game_profile_id: friend_profile_id, confirmed: true)
+      Friendship.create(user_id: friend.id, friend_id: user.id, block_game_profile_id: friend_profile_id, friend_block_game_profile_id: profile_id, accepted: true)
+    end
+  end
+
+  def inverse_friendship
+    Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
+  end
+
+  def accept_friendship
+    if self.update(confirmed: true, rejected: false, accepted: true)
+      self.inverse_friendship.update(accepted: true)
+    end
+  end
+
+  def reject_friendship
+    if self.update(confirmed: true, rejected: true, accepted: false)
+      self.inverse_friendship.update(accepted: false)
+    end
+  end
+
+  private
+
+  def not_friends_with_self
+    errors.add(:user_id, "can't be friends with self") if user_id == friend_id
+  end
+
+  def set_confirmation
+    self.confirmed_at = confirmed ? DateTime.now : nil if confirmed_changed? || rejected_changed?
+  end
+end
