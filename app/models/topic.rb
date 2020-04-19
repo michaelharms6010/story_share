9ee450cdef4096903d71fdacf44ceec3b552e13f
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-19 12:10:02 -0400

    Topic model

---
+++ app/models/topic.rb
@@ -0,0 +1,10 @@
+class Topic < ActiveRecord::Base
+
+  default_scope -> { order(published_at: :desc) }
+  validates :text, presence: true
+  validates :prompt, presence: true, length: { minimum: 10, maximum: 255 },
+                    uniqueness: { case_sensitive: false }
+
+  private
+
+end
