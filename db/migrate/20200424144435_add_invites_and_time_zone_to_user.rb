dd49f056e846c276807104e0daba7b46ec8d37cc
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-24 10:53:26 -0400

    Invite migration

---
+++ db/migrate/20200424144435_add_invites_and_time_zone_to_user.rb
@@ -0,0 +1,15 @@
+class AddInvitesAndTimeZoneToUser < ActiveRecord::Migration[6.0]
+  def change
+
+    add_column :users, :first_name, :string, null: false, default: ""
+    add_column :users, :last_name,  :string, null: false, default: ""
+
+    add_column :users, :time_zone, :string, null: false, default: "Pacific Time (US & Canada)"
+
+    add_column :users, :show_full_name, :boolean, null: false, default: false
+
+    add_column :users, :invited_by, :integer
+    add_index  :users, :invited_by
+
+  end
+end
