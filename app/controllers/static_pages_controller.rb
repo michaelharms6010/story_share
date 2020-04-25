857439542e9a68c35916ffb27cafd9cf9019d6c8
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-25 13:07:00 -0400

    Fixing login loop

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
 
-  before_action :logged_in_user,   only: [:home]
-
   def home
     @user = current_user
-    @stories = @user.all_stories
+    if @user.present?
+      @stories = @user.all_stories
+    end
   end
 end
