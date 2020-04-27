b81575cdbd73adcc3dd6475b9cd8d41948bac523
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-27 18:52:02 -0400

    Defaults

--- db/migrate/20200427224839_add_streak_and_guest_to_user.rb
+++ db/migrate/20200427224839_add_streak_and_guest_to_user.rb
@@ -1,8 +1,8 @@
 class AddStreakAndGuestToUser < ActiveRecord::Migration[6.0]
   def change
 
-    add_column :users, :streak, :integer
-    add_column :users, :is_guest, :boolean
+    add_column :users, :streak, :integer, null: false, default: 0
+    add_column :users, :is_guest, :boolean, null: false, default: false
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2020_04_27_224839) do
     t.string "time_zone", default: "Pacific Time (US & Canada)", null: false
     t.boolean "show_full_name", default: false, null: false
     t.integer "invited_by"
-    t.integer "streak"
-    t.boolean "is_guest"
+    t.integer "streak", default: 0, null: false
+    t.boolean "is_guest", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invited_by"], name: "index_users_on_invited_by"
     t.index ["name"], name: "index_users_on_name", unique: true
