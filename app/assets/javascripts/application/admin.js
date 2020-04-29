6fe99a34b8f1d4f0f73b756a7cbf472437490eb8
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-04-29 12:40:42 -0400

    Focus debug

--- app/assets/javascripts/application/admin.js
+++ app/assets/javascripts/application/admin.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 
   if ($(".js-debug-stats").length > 0) {
 
-    $(".js-debug-stats").html("Window width: " + window.innerWidth);
+    $(".js-debug-stats").html("Window width: " + window.innerWidth + ", " + document.activeElement);
 
     if ($(".js-story-timer-start").length > 0) {
       $("div").on("click", function() {
         console.log($(this).html())
-        $(".js-debug-stats").html($(".js-debug-stats").html() + "</br>" + $(this).attr("class"));
+        $(".js-debug-stats").html($(".js-debug-stats").html() + "</br>" + $(this).attr("class") + ", " + document.activeElement);
       })
     }
   }
