c0a6f3b73e9503c0106098874b9968ce8767e63c
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-13 21:55:07 -0700

    Add mailer reply

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   require 'mail'
 
-  address = Mail::Address.new("noreply@br80.io")
+  address = Mail::Address.new("noreply@storyfriends.net")
   address.display_name = "Story Friends"  # Appears as the sendee's name
   default from: address.format
 
