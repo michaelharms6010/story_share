e7fdcb781e2857110ebc03875902555f0856fb41
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-26 13:43:08 -0700

    Revert migration

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,8 @@
+class CommentsController < ApplicationController
+  before_action :logged_in_user,   only: [:index]
+
+  def index
+
+  end
+
+end
--- db/migrate/20200526165613_add_notifications_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddNotificationsToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column    :users, :notifications, :jsonb, null: false, default: {}
-  end
-end
