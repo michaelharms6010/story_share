c81c257f9099f69127943814c81660ec6710a024
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-26 09:55:34 -0700

    Revert comment

--- db/migrate/20200526033410_add_viewed_to_comment.rb
+++
@@ -1,8 +0,0 @@
-class AddViewedToComment < ActiveRecord::Migration[6.0]
-  def change
-
-    add_column :comments, :viewed,  :boolean, null: false, default: false
-    add_index :comments, :viewed
-
-  end
-end
