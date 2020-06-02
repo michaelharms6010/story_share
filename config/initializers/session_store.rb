5f3a2056b5db73577fc8cfa21cf23f22821241be
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-06-02 10:32:22 -0700

    Revert

--- config/initializers/session_store.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-StoryShare::Application.config.session_store :cookie_store,  {
-  key:   '_story_share_session',
-  :expire_after =>   5.minutes,
-  :secure =>         true
-  #:httponly =>      true,
-  #:cookie_only =>   true
-}
