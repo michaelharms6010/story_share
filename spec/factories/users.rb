1778fda79255076d906f2d05eff7b169b84d12fb
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-06-04 11:56:31 -0700

    Add factorybot init

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Internet.username(specifier: 3..20, separators: %w(_ -)) }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    name { "TestName" }
+    email { "test@example.com" }
+    password { "Password123" }
   end
+
+  # factory :user do
+  #   name { Faker::Internet.username(specifier: 3..20) }
+  #   email { Faker::Internet.email }
+  #   password { Faker::Internet.password }
+  # end
 
   trait :admin do
     admin {true}
